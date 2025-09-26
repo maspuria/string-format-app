8e50577949210d2f2d522aa3c7a26692091ffd78
mariannaaspuria <aspuria.mp@gmail.com>
2025-09-25 20:43:13 -0500

    practice commit

---
+++ src/main/java/com/example/FormatPractice.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class FormatPractice {
+    public static void main(String[] args) {
+        String iconsName = "Lady Gaga";
+        int numberOfGrammys = 14;
+        double iconicMeatDressWeight =  22.68;
+
+        // Step 1 : Format and print the string variable (icons name)
+        String artistOfTheYear = ("The Artist of the Year for 2025: %s", iconsName);
+        System.out.println(artistOfTheYear);
+
+    }
+}
--- src/main/java/com/example/FormattingExercise.java
+++ src/main/java/com/example/FormattingExercise.java
@@ -5,13 +5,13 @@ public class FormattingExercise {
         String name = "Alice";
         int age = 25;
         double gpa = 3.6789;
-        // Step 1
+        // Step 1: Format and print the string variable (name)
         String formatName = String.format("Name: %s", name);
         System.out.println(formatName);
-        // Step 2: format and print the int variable age
+        // Step 2: format and print the int variable (age)
         String formatAge = String.format("Age: %d", age);
         System.out.println(formatAge);
-        //step 3: format and print the double variable gpa with 2 decimal places
+        //step 3: format and print the double variable (gpa) with 2 decimal places
         String formatGpa = String.format("GPA: %.2f", gpa);
         System.out.println(formatGpa);
         // step 4 combine all three into one formatted sentence using String.format
